e9687ae77ee5ec1f819c96b44de21db5b9a4ceb7
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-29 09:34:23 -0800

    Commit 1

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,6 +26,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+
+        const token = signToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -36,5 +40,21 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function signToken(user) {
+  const payload = {
+    userId = user.id,
+    username = user.username
+  };
+
+  const secret = "snitches get stitches!";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+};
+
 
 module.exports = router;
